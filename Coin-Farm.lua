68515e26e94b0b4e3c0566fe0542fe66a1250bd0
samuraa1 <artemivanisovlol@gmail.com>
2025-08-10 17:40:18 +0300

    Update Coin-Farm.lua

--- Coin-Farm.lua
+++ Coin-Farm.lua
@@ -1,7 +1,7 @@
 local RS = game:GetService('ReplicatedStorage')
 local JobId = game.JobId
 local PlaceId = game.PlaceId
-local Remote = RS:WaitForChild("ClaimReward")
+local Remote = game:GetService("ReplicatedStorage"):WaitForChild("ClaimReward")
 local TeleportService = game:GetService("TeleportService")
 local function Fire(obj)
     obj:FireServer()
