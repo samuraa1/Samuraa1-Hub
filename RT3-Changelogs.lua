d71b1322c627d94b6954fe97539283d3ce855931
samuraa1 <artemivanisovlol@gmail.com>
2025-09-20 13:31:31 +0300

    Update RT3-Changelogs.lua

--- RT3-Changelogs.lua
+++ RT3-Changelogs.lua
@@ -4,17 +4,7 @@ Library:CreateChangelogDialog({
     Title = "Restaurant Tycoon 3 Script - Changelogs",
     ChangelogText = [[
         Version: 1.1.0
-        + Added Feedback In Home Tab
-+ Set FPS Default FPS Is 120 Now
-
-+ Autofarms Is Very Fast Now!
-+ Added 'Select Everything' and 'Select Nothing' Buttons For Auto Farm And Restaurant Upgrades GroupBoxes
-+ Added Toggle For 'Claim Daily Reward' And Renamed To 'Auto Claim Daily Reward' + Added Auto Collect Objectives
-+ Fixed Collect All Scavenger Hunt Items
-
-+ Added Farm Tab (Nothing In It Right Now)
-
-+ Removed 'Inf Camera Zoom' Button In Local Player Tab, Added 'Reset Camera Zoom' Button + Changed Keybind For Fly
+        + Auto Seat Customers Is Works Now On Mobiles
     ]],
     Search = false,
     NotifyLibrary = "Luna",
