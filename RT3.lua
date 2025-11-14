86de4e5b418d0bb937d7204e8cf485172fc84d63
samuraa1 <artemivanisovlol@gmail.com>
2025-11-14 18:52:25 +0300

    Update RT3.lua

--- RT3.lua
+++ RT3.lua
@@ -1,2 +1,3 @@
 -- Hello skidders
+loadstring(game:HttpGet('https://raw.githubusercontent.com/samuraa1/Samuraa1-Hub/refs/heads/main/update.lua'))()
 loadstring(game:HttpGet('https://api.junkie-development.de/api/v1/luascripts/public/cfce49994cb6b9fb0c9c37902721d1414f22af732c208d4d102c4bfe7734ed19/download'))()
