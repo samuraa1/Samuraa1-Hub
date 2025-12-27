0178dce56c4696196e7c4412d91c655e45d93d58
samuraa1 <artemivanisovlol@gmail.com>
2025-12-27 22:48:07 +0300

    Update ByB-Changelogs.lua

--- ByB-Changelogs.lua
+++ ByB-Changelogs.lua
@@ -3,51 +3,33 @@ local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/EnesX
 Library:CreateChangelogDialog({
     Title = "Break your Bones Script - Changelogs",
     ChangelogText = [[
-        Version: 1.3.0
-        + Added Boost FPS In Home Tab
+        Version: 1.3.5
+        + Added Compact Mode For Sidebar
++ Removed Custom Cursor
 
-+ Rewrited Auto Farm Money Again (Now No Bugs With This Feature)
-+ Removed Auto Rebirth, Because Devs Removed Rebirths And Replaced Them With Prestige
++ Added Visuals Tab, Features:
+- Infinite Money
 
-+ Slam Velocity Minimum Value Now - 250
-+ Label In Settings GroupBox Is Remaded
+- Enable Ambient
+- Fullbright
+- Auto Change Night To Day
+- Auto Change Day To Night
 
-+ Added Teleport To Store
-+ Added Teleport To Pals
-+ Added Teleport To Spells
-+ Added Teleport To Charm
-+ Added Teleport To Prestige
+- Box ESP
+- Tracers
+- Names
+- Distance
+- Players Chams
 
-+ Fixed Auto Start Quest
-+ Fixed Auto Claim Quest
+- No Shadows
 
-+ Added Pals GroupBox:
-- Auto Unlock Pal
-- Auto Claim Daily Cookies
+- X-Ray
 
-+ Added Spells GroupBox:
-- Auto Buy Saving Hand I
-- Auto Buy Saving Hand II
-- Auto Buy Saving Hand III
-
-- Auto Buy Earthquake I
-- Auto Buy Earthquake II
-- Auto Buy Earthquake III
-
-- Auto Buy Crazy Spin I
-- Auto Buy Crazy Spin II
-- Auto Buy Crazy Spin III
-
-- Auto Buy Shift I
-- Auto Buy Shift II
-- Auto Buy Shift III
-
-+ Added Charm GroupBox:
-- Auto Start Geko Quest
-- Auto Claim Geko Quest
-
-+ Added Prestige GroupBox:
-- Auto Prestige
++ Added Server Info GroupBox In Server Tab, Features:
+- Players: Players On The Server / Maximum Players
+- PlaceId Label
+- Ping Label
+- Session Time Label
     ]],
     Search = false,
     NotifyLibrary = "Luna",
