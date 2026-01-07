c0dad649375324aa56601c54fd1915c40e18b1ce
samuraa1 <artemivanisovlol@gmail.com>
2026-01-07 15:30:50 +0300

    Update RT3-Changelogs.lua

--- RT3-Changelogs.lua
+++ RT3-Changelogs.lua
@@ -3,40 +3,19 @@ local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/EnesX
 Library:CreateChangelogDialog({
     Title = "Restaurant Tycoon 3 Script - Changelogs",
     ChangelogText = [[
-        Version: 1.5.0
-        + Added Boost FPS In Home Tab
+        Version: 1.5.1
+        + Updated Feedback
 
-+ Added Auto Collect Drop Money/Food Critic
-+ Change Restaurant Name
-+ Updated Redeem All Valid Codes
-+ Added Info Label For Restaurant Upgrades
-+ Added Franchise Restaurant Upgrade
++ Added Logo
++ Updated Footer
++ Updated UI Size
++ Added Compact Mode For Sidebar
 
-+ Added Features In Farm Tab:
-- Select Crops to Plant
-- Auto Plant Crops
-- Auto Harvest Crops
-- Destroy All Crops
-
-- Teleport to Farm In This Tab Now
-
-+ Added Visuals Tab, Features:
-- Infinite Cash
-- Infinite Cash On Leaderstats
-- Infinite Diamonds
-
-- Change Ambient
-- Fullbright
-- Auto Change Night To Day
-- Auto Change Day To Night
-
-- Players Chams
-- No Fog
-
-+ Added Teleport To Private Server
-
-+ Another UI Default Size
-+ Disabled The Custom Cursor
++ Added Server Info GroupBox In Server Tab, Features:
+- Players: Players On The Server / Maximum Players
+- PlaceId Label
+- Ping Label
+- Session Time Label
     ]],
     Search = false,
     NotifyLibrary = "Luna",
