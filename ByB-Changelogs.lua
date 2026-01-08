05d2f98458bc55c6f6264490fcaddfdd18bd12bf
samuraa1 <artemivanisovlol@gmail.com>
2026-01-08 22:32:31 +0300

    Update ByB-Changelogs.lua

--- ByB-Changelogs.lua
+++ ByB-Changelogs.lua
@@ -3,33 +3,13 @@ local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/EnesX
 Library:CreateChangelogDialog({
     Title = "Break your Bones Script - Changelogs",
     ChangelogText = [[
-        Version: 1.3.5
-        + Added Compact Mode For Sidebar
-+ Removed Custom Cursor
+        Version: 1.3.6
+        + Added Logo
++ Updated Footer
++ Updated UI Size
 
-+ Added Visuals Tab, Features:
-- Infinite Money
-
-- Enable Ambient
-- Fullbright
-- Auto Change Night To Day
-- Auto Change Day To Night
-
-- Box ESP
-- Tracers
-- Names
-- Distance
-- Players Chams
-
-- No Shadows
-
-- X-Ray
-
-+ Added Server Info GroupBox In Server Tab, Features:
-- Players: Players On The Server / Maximum Players
-- PlaceId Label
-- Ping Label
-- Session Time Label
++ Added Label In Visuals Tab
++ ESP GroupBox Is Right GroupBox Now
     ]],
     Search = false,
     NotifyLibrary = "Luna",
