b38e2ac76b94388aa937e342125d9a518fb87b9c
samuraa1 <artemivanisovlol@gmail.com>
2026-02-15 23:26:03 +0300

    Update RT-Changelogs.lua

--- RT-Changelogs.lua
+++ RT-Changelogs.lua
@@ -3,15 +3,44 @@ local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/EnesX
 Library:CreateChangelogDialog({
     Title = "Raft Tycoon Script - Changelogs",
     ChangelogText = [[
-        Version: 1.0.6
-        + Added Logo
-+ Updated Footer
-+ Updated UI Size
+        Version: 1.1.0
+        + Updated Feedback
 
-+ Removed Presents ESP
++ Renamed 'Auto Farm' GroupBox to 'Automation'
+
++ Optimized Auto Collect Money
++ Rewrited Auto Buy - Optimized + Added Check How Many Money U Have (If U Doesnt Have Money To Buy Button - Button Will Not Be Teleported To U) + Button Now Teleporting Near Head
+
++ Rewrited Walk On Water - No More Bugs
++ Added Auto Complete Parkour
 
 + All Codes In 'Redeem All Valid Codes' Are Working
-+ Added Info Label About Codes
+
++ Added Teleports GroupBox, Features:
+- Teleport to Spawn
+- Teleport to Leaderstats
+- Teleport to Parkour
+- Teleport to PVP + Mob
+
++ Rewrited Fly - Added Mobile Support + Fly Keybind Is F Now + Another Method + Way Better Now
+
++ Added Visuals Tab, Features:
+- Infinite Money
+
+- Enable Ambient
+- Fullbright
+
+- Box ESP
+- Tracers
+- Names
+- Distance
+- Players Chams
+
+- No Shadows
+
+- X-Ray
+
++ Added Auto Execute
     ]],
     Search = false,
     NotifyLibrary = "Luna",
