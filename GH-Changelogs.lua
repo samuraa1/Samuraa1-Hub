df5b905e2b468d800177716420618b32e6091e04
samuraa1 <artemivanisovlol@gmail.com>
2026-03-10 20:31:22 +0300

    Update GH-Changelogs.lua

--- GH-Changelogs.lua
+++ GH-Changelogs.lua
@@ -3,59 +3,13 @@ local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/EnesX
 Library:CreateChangelogDialog({
     Title = "Garden Horizons Script - Changelogs",
     ChangelogText = [[
-        Version: 2.5.0
-Main Tab:
-+ Rewrited Auto Plant - Some Optimization, Added Check If You Have New Seed In Inventory (To Not Restart Auto Plant Toggle), Bug Fixes. So Now Auto Plant Works Perfectly Fine!
-+ Added Plant Position Method: Randomized and Custom Set Position
-If Custom Set Position Enabled U Will See:
-- Get Current Position and Custom Position Input
-+ Removed Info About Auto Plant Because Now Its Planting Instanly
-
-+ Added Auto Water + Many Settings For This Feature
-+ Added Auto Sprinkler + Many Settings For This Feature
-
-+ Added Tree/Plant Shovel + Many Settings For This Feature
-+ Added Fruit Shovel + Many Settings For This Feature
-
-
-+ Added 'Backpack' Tab, Features:
-+ Sell Feature Now In This Tab + Added Skip Favorited In Specific Mode
-
-- Auto Favorite (auto favorite only plants that in backpack, not that on ur garden)
-- Plants to Favorite
-- Mutations
-
-- Unfavorite Divider
-- Plants to Unfavorite
-- Refresh Lish
-- Unfavorite Selected Plants
-
-
-Shops Tab:
-+ Added Tooltips
-+ Now Delay Between Buys For 'Auto Buy Seeds' and 'Auto Buy Gears' Default Is 50 ms
-+ Rewrited 'Auto Buy Seeds' and 'Auto Buy Gears'
-
-Miscellaneous Features Tab:
-+ Added Set AFK Status (FE)
-
-+ Redeem All Valid Codes: Removed Expired Codes + Removed Info Label + Added Code - CONSOLE
-
-+ Added More Teleports
-
-+ Rewrited Always Render Plot - No More Bugs
-
-+ Added Auto Refresh Daily Quests
-+ Added Auto Refresh Weekly Quests
-
-Server Tab:
-+ Added Divider
-
-+ Added Info Tab About Visuals and Server Tabs
-
-UI Changes:
-+ Fixed Warns In Console About Theme
-+ Updated UI Size
+        Version: 2.5.3
+Manny Mishap Event:
++ Added For Auto Collect Fruits - Anti Sit + Auto Delete Fountain TouchInterest (that throws u)
++ Added Stop Collect If Reached 300 Points
++ Added Only Submit When You Have 300 Points
++ Added Dividers
++ Fixed Bugs
     ]],
     Search = false,
     NotifyLibrary = "Luna",
