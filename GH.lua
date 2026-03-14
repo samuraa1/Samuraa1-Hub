71666179c04cb8cbdc8c403620e001808b9e719e
samuraa1 <artemivanisovlol@gmail.com>
2026-03-14 17:07:31 +0300

    Update GH.lua

--- GH.lua
+++ GH.lua
@@ -1,1 +1,4 @@
+-- Info About Update
+loadstring(game:HttpGet('https://raw.githubusercontent.com/samuraa1/Samuraa1-Hub/refs/heads/main/update_gh.lua'))()
+---
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/b06c092970c62cdd223faed0bbaca94d0c0add8a2e3a1e8cf66b2a469f9ccebf/download"))()
