b84b3fcaa312ad8b9c03ac4f7f36316070882e2e
samuraa1 <artemivanisovlol@gmail.com>
2026-04-01 00:14:41 +0300

    Update RT-Changelogs.lua

--- RT-Changelogs.lua
+++ RT-Changelogs.lua
@@ -3,44 +3,13 @@ local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/EnesX
 Library:CreateChangelogDialog({
     Title = "Raft Tycoon Script - Changelogs",
     ChangelogText = [[
-        Version: 1.1.0
-        + Updated Feedback
+        Version: 1.5.0
+        + New UI!
 
-+ Renamed 'Auto Farm' GroupBox to 'Automation'
-
-+ Optimized Auto Collect Money
-+ Rewrited Auto Buy - Optimized + Added Check How Many Money U Have (If U Doesnt Have Money To Buy Button - Button Will Not Be Teleported To U) + Button Now Teleporting Near Head
-
-+ Rewrited Walk On Water - No More Bugs
-+ Added Auto Complete Parkour
++ Rewrited Auto Collect Money & Auto Buy - Now Parts To Collect Money Or Buy Doesn't Teleport To You (working with just firetouchinterest)
 
 + All Codes In 'Redeem All Valid Codes' Are Working
-
-+ Added Teleports GroupBox, Features:
-- Teleport to Spawn
-- Teleport to Leaderstats
-- Teleport to Parkour
-- Teleport to PVP + Mob
-
-+ Rewrited Fly - Added Mobile Support + Fly Keybind Is F Now + Another Method + Way Better Now
-
-+ Added Visuals Tab, Features:
-- Infinite Money
-
-- Enable Ambient
-- Fullbright
-
-- Box ESP
-- Tracers
-- Names
-- Distance
-- Players Chams
-
-- No Shadows
-
-- X-Ray
-
-+ Added Auto Execute
+and etc.
     ]],
     Search = false,
     NotifyLibrary = "Luna",
