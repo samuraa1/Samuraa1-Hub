b199170038956e05f7eb4411072ee90d3736a3e9
samuraa1 <artemivanisovlol@gmail.com>
2026-05-08 20:44:04 +0300

    Update Expedition-Antarctica-Changelogs.lua

--- Expedition-Antarctica-Changelogs.lua
+++ Expedition-Antarctica-Changelogs.lua
@@ -1,44 +1,12 @@
-local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/EnesXVC/librarys/main/SearchAndChangeLogLibrary/Source'))()
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/samuraa1/Samuraa1-Hub/refs/heads/main/SearchAndChangeLogLibrary/Source'))()
 
 Library:CreateChangelogDialog({
     Title = "Expedition Antarctica Script - Changelogs",
     ChangelogText = [[
-        Version: 3.1.0
-+ Some Optimizations
-
-+ Added Logo
-+ Added Compact Mode For Sidebar
-+ Removed Custom Cursor
-+ Updated Footer
-+ Updated UI Size
-+ Added Another Default Theme
-
-+ Changed Icon For Welcome GroupBox
-+ Added Feedback In Home Tab
-+ Added Boost FPS In Home Tab
-
-+ Removed Inf Camera Zoom
-+ Added Reset Camera Zoom
-
-+ Fully Rewrited Auto Farm Expeditions:
-+ Added Loop Count
-+ Added Jump Pause Before Teleport
-+ Added Jump Resume After Teleport
-+ Added Waiting Time At Spawn
-
-+ Added Infinite Coins
-+ Added Auto Change Day To Night
-
-+ Added Info Label About Coin Farm In Vulnerabilities Tab
-+ Added Dividers In Boosts GroupBox
-
-+ Added Server Info GroupBox In Server Tab, Features:
-- Players: Players On The Server / Maximum Players
-- PlaceId Label
-- Ping Label
-- Session Time Label
-
-+ Updated Executions Tracker
+        Version: 3.5.0
++ New UI
++ Removed Noclip (detected)
+etc.
     ]],
     Search = false,
     NotifyLibrary = "Luna",
