97b82c0d169c6083975cdbef78df2ad2534d0012
samuraa1 <artemivanisovlol@gmail.com>
2026-05-08 22:50:24 +0300

    Delete MPT.lua

--- MPT.lua
+++
@@ -1,4 +0,0 @@
-local args = {
-    [1] = 10000000000
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Honeypot"):WaitForChild("Internal"):WaitForChild("RemoteStorage"):WaitForChild("GiveLuckyBlockReward - RemoteEvent"):FireServer(unpack(args))
