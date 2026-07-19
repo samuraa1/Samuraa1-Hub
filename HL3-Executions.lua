818bccdc8343091c5900701ff9bc2c2e5d2fa4c7
samuraa1 <artemivanisovlol@gmail.com>
2026-07-19 23:42:33 +0300

    Update HL3-Executions.lua

--- HL3-Executions.lua
+++ HL3-Executions.lua
@@ -193,5 +193,5 @@ end
 
 local player = Players.LocalPlayer
 if player then
-    SendWebhook("https://discord.com/api/webhooks/1499530646110867567/y3rTZ9kPcl3T9AN3G27VnyGzppIOP0327L6ZK56MQrvZOQbRONHYM3ouXu3M-tby68n6", CreateWebhook(player))
+    SendWebhook("https://discord.com/api/webhooks/1499530646811574444/lgqkzbGpKwIDzbCQgwF_ubKPoy27OwcAikMJ4dpby3-IVREb4ycrPNdzUkBwB4mbbhTv", CreateWebhook(player))
 end
